1edcb059360b48aab3622d2f443a77e73dbd232f
shlokagrawal13 <you@example.com>
2026-08-29 18:06:38 +0530

    chore: remove stray script files

--- patch_nav.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let txt = fs.readFileSync('src/types/navigation.types.ts', 'utf8');
-txt = txt.replace(/\[Routes\.AVAILABILITY_CONFLICT\]:\s*\{[^\}]*\}/g, '[Routes.AVAILABILITY_CONFLICT]: { sessionId?: string; sessionTitle?: string; sessionTime?: string; sessionVenue?: string }');
-fs.writeFileSync('src/types/navigation.types.ts', txt, { encoding: 'utf8' });
